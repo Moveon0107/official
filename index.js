ff0d673694e42ab78d8bee2dd6d84ee4a0603387
Moveon0107 <141475274+Moveon0107@users.noreply.github.com>
2023-12-05 10:27:14 +0900

    축제 이메일 인증

--- index.js
+++ index.js
@@ -11,6 +11,48 @@ const port = 3000;
 
 app.use(bodyParser.raw({ type: 'image/jpeg', limit: '10mb' }));
 
+//2023 대현고 비상제 축제
+app.post('/EmailSend', function (req, res) {
+  const nodemailer = require('nodemailer');
+
+  const transporter = nodemailer.createTransport({
+    service: 'Gmail',
+    auth: {
+      user: 'email.address.confirm1@gmail.com', // 본인의 Gmail 주소 입력
+      pass: 'irsv azkh sgmr idua' // 생성한 앱 비밀번호 입력
+    }
+  });
+
+  const { nickname, email } = req.body;
+  Alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  alphabet = "abcdefghijklmnopqrstuvwxyz";
+  verify[email] = "";
+  verify[email] += Math.floor(Math.random() * 2) == 0 ? Math.floor(Math.random() * 2) == 0 ? Math.floor(Math.random() * 10) : Alphabet[Math.floor(Math.random() * Alphabet.length)] : Math.floor(Math.random() * 2) == 0 ? Math.floor(Math.random() * 10) : alphabet[Math.floor(Math.random() * alphabet.length)] + '';
+  verify[email] += Math.floor(Math.random() * 2) == 0 ? Math.floor(Math.random() * 2) == 0 ? Math.floor(Math.random() * 10) : Alphabet[Math.floor(Math.random() * Alphabet.length)] : Math.floor(Math.random() * 2) == 0 ? Math.floor(Math.random() * 10) : alphabet[Math.floor(Math.random() * alphabet.length)] + '';
+  verify[email] += Math.floor(Math.random() * 2) == 0 ? Math.floor(Math.random() * 2) == 0 ? Math.floor(Math.random() * 10) : Alphabet[Math.floor(Math.random() * Alphabet.length)] : Math.floor(Math.random() * 2) == 0 ? Math.floor(Math.random() * 10) : alphabet[Math.floor(Math.random() * alphabet.length)] + '';
+  verify[email] += Math.floor(Math.random() * 2) == 0 ? Math.floor(Math.random() * 2) == 0 ? Math.floor(Math.random() * 10) : Alphabet[Math.floor(Math.random() * Alphabet.length)] : Math.floor(Math.random() * 2) == 0 ? Math.floor(Math.random() * 10) : alphabet[Math.floor(Math.random() * alphabet.length)] + '';
+  verify[email] += Math.floor(Math.random() * 2) == 0 ? Math.floor(Math.random() * 2) == 0 ? Math.floor(Math.random() * 10) : Alphabet[Math.floor(Math.random() * Alphabet.length)] : Math.floor(Math.random() * 2) == 0 ? Math.floor(Math.random() * 10) : alphabet[Math.floor(Math.random() * alphabet.length)] + '';
+
+  const mailOptions = {
+    from: 'email.address.confirm1@gmail.com', // 발신자 이메일
+    to: email, // 수신자 이메일
+    subject: '이메일 인증',
+    html: `안녕하세요! ${nickname}님,<br>아래 인증코드를 입력하여 회원가입을 완료하세요.<div style="display: flex; justify-content: center;"><span style="background-color: dodgerblue; color: #fff; padding: 20px; border-radius: 15px; letter-spacing: 5px; font-weight: bold; margin-top: 50px;">${verify[email]}</span></div>`
+  };
+  // 이메일 보내기
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      console.error('이메일 발송 실패:', error);
+      res.status(500).json({ success: false, message: '이메일 발송 실패' });
+    } else {
+      console.log('이메일 발송 성공:', info.response);
+      res.json({ success: true, message: '이메일 발송 성공' });
+    }
+  });
+
+});
+
+//교육청 해커톤
 app.post('/cam', (req, res) => {
   // POST 요청에서 프레임 데이터를 받아서 파일로 저장
   if (req.body && req.body.length > 0) {
